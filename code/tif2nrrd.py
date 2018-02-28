357ab2ae550fa5e69bccb3e22312b57de2bff748
Valentina Staneva <11621647+valentina-s@users.noreply.github.com>
2018-02-28 15:14:12 -0800

    adding a bit of comments

--- code/tif2nrrd.py
+++ code/tif2nrrd.py
@@ -1,3 +1,13 @@
+"""
+    tif2nrrd.py converts tif format to nrrd for slice visualization.
+    It provides the option to to downsample the image.
+
+    Usage:
+    python tif2nrrd infile.tif outfile.nrrd 0.25
+
+    The downsampling factor above is 0.5.
+"""
+
 import sys
 from skimage.external.tifffile import imread
 from skimage.transform import resize
@@ -6,10 +16,14 @@ import numpy as np
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
-zoom_factor = float(sys.argv[3])
+
+if len(sys.argv)<4:
+    zoom_factor = 1
+else:
+    zoom_factor = float(sys.argv[3])
 
 im = np.array(imread(infile)[0])
 im_small = resize(im,tuple(np.array(im.shape)*zoom_factor), mode='constant')
-print(im_small.shape)
-print(im.shape)
-nrrd.write(outfile,im_small)
+print('Original image dimensions: '+ str(im.shape))
+print('Downsampled image dimensions: '+ str(np.squeeze(im_small).shape))
+nrrd.write(outfile,np.squeeze(im_small))
