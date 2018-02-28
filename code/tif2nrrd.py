fd91eba33d4708adfdf1796d4b6940c9b593849a
Valentina Staneva <11621647+valentina-s@users.noreply.github.com>
2018-02-28 14:58:53 -0800

    fixing zoom factor format

--- code/tif2nrrd.py
+++ code/tif2nrrd.py
@@ -9,5 +9,7 @@ outfile = sys.argv[2]
 zoom_factor = float(sys.argv[3])
 
 im = np.array(imread(infile)[0])
-im_small = resize(im,tuple(np.ones(len(im.shape))*zoom_factor), mode='constant')
+im_small = resize(im,tuple(np.array(im.shape)*zoom_factor), mode='constant')
+print(im_small.shape)
+print(im.shape)
 nrrd.write(outfile,im_small)
