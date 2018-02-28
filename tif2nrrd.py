41097bd2c233d5cae372e9567543facac87c2829
Valentina Staneva <11621647+valentina-s@users.noreply.github.com>
2018-02-28 10:50:46 -0800

    adding tif2nrrd conversion

---
+++ tif2nrrd.py
@@ -0,0 +1,13 @@
+import sys
+from skimage.external.tifffile import imread
+from skimage.transform import resize
+import nrrd
+import numpy as np
+
+infile = sys.argv[1]
+outfile = sys.argv[2]
+zoom_factor = float(sys.argv[3])
+
+im = np.array(imread(infile)[0])
+im_small = resize(im,tuple(np.ones(len(im.shape))*zoom_factor), mode='constant')
+nrrd.write(outfile,im_small)
